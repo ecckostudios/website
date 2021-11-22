e87eb1bfbf4ffa090459880542c525585b7f42f1
João Pedro <jpbgama@gmail.com>
2021-11-22 17:47:26 -0300

    Dia22Nov

--- function.js
+++ function.js
@@ -1,1 +1,16 @@
+var nome = document.getElementById("nome");
+var email = document.getElementById("whatsapp");
+var whatsapp = document.getElementById("email");
+var mensagem = document.getElementById("mensagem");
+var btnEnviar = document.getElementById("enviar");
+var contato = document.getElementById("contato");
 
+
+contato.addEventListener ("submit", event=> {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const info = Object.fromEntries(formData);
+    console.log(info)
+    alert("Mensagem Enviada com sucesso");
+});
